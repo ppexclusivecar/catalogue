6e62b87ea0f792b434869e2c9105cb709c1c93b4
ElrumbisDev <eliott.moores@gmail.com>
2024-10-29 15:15:21 +0100

    multer correction

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
-// backend/index.js
 const express = require('express');
 const cors = require('cors');
-const catalogueRoutes = require('./routes/catalogue');
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' }); // Utilisé temporairement pour multer
 require('dotenv').config();
 
 const app = express();
@@ -17,44 +14,33 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+// Utiliser le stockage en mémoire avec Multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Servir les fichiers statiques du dossier 'public' pour d'autres assets, si besoin
-app.use('/public', express.static('public'));
-
-// Route racine
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur le backend Express!');
-});
-
-// Exemple de route API
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Bonjour depuis le backend Express!' });
-});
-
-app.post('/api/data', (req, res) => {
-  const data = req.body;
-  res.json({ message: 'Données reçues', data });
-});
-
 // Route d'upload d'image vers Cloudinary
 app.post('/api/upload', upload.single('image'), async (req, res) => {
   try {
-    // Uploader l'image vers Cloudinary
-    const result = await cloudinary.uploader.upload(req.file.path);
-
-    // Renvoyer l'URL sécurisée de l'image
-    res.json({ imageUrl: result.secure_url });
+    // Uploader l'image vers Cloudinary à partir du buffer
+    const result = await cloudinary.uploader.upload_stream(
+      { resource_type: 'image' },
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({ error: 'Erreur lors de l\'upload de l\'image' });
+        }
+        // Renvoyer l'URL sécurisée de l'image
+        res.json({ imageUrl: result.secure_url });
+      }
+    ).end(req.file.buffer); // Utiliser le buffer pour uploader l'image
   } catch (error) {
     console.error('Erreur lors de l\'upload vers Cloudinary:', error);
     res.status(500).json({ error: 'Erreur lors de l\'upload de l\'image' });
   }
 });
-
-// Routes du catalogue
-app.use('/api/catalogue', catalogueRoutes);
 
 // Démarrer le serveur
 app.listen(PORT, () => {
