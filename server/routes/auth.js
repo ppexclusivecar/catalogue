a83d091c34ec15e4f728aadc67ac8133307047a5
ElrumbisDev <eliott.moores@gmail.com>
2024-11-01 16:38:57 +0100

    backend .env

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,16 +7,16 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 // Endpoint de connexion pour obtenir un token JWT
 router.post('/login', (req, res) => {
-  const { password } = req.body;
-
-  // Vérification du mot de passe
-  if (password === ADMIN_PASSWORD) {
-    // Crée un token JWT valable pendant 1 heure
-    const token = jwt.sign({ role: 'admin' }, JWT_SECRET, { expiresIn: '1h' });
-    return res.json({ token });
-  } else {
-    return res.status(401).json({ message: 'Mot de passe incorrect' });
-  }
-});
+    const { password } = req.body;
+    console.log('Mot de passe reçu:', password);  // Vérifiez ici le mot de passe reçu
+    console.log('Mot de passe attendu:', ADMIN_PASSWORD);  // Vérifiez le mot de passe attendu
+  
+    if (password === ADMIN_PASSWORD) {
+      const token = jwt.sign({ role: 'admin' }, JWT_SECRET, { expiresIn: '1h' });
+      return res.json({ token });
+    } else {
+      return res.status(401).json({ message: 'Mot de passe incorrect' });
+    }
+  });
 
 module.exports = router;
