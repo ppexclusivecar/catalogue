b07144c5f998c23844e8d2970a715d39dc0aa316
ppexclusivecar <webmasterppexclusive@gmail.com>
2025-01-09 11:05:09 +0100

    title site

--- client/data/index.tsx
+++ client/data/index.tsx
@@ -9,28 +9,28 @@ export const navItems = [
 ]
   export const testimonials = [
     {
-      quote: "J'ai vendu ma voiture en moins d'une semaine ! L'équipe a été super réactive et m'a accompagné tout au long du processus. Un service rapide, transparent et sans stress. Je recommande vivement !",
-      name: "Lucas Moreau",
+      quote: "« J’ai acheté une Golf 7 GTI performance chez Pierre, le gérant de p&p exclusive car. Je suis entièrement satisfait de mon achat. Pierre a été réactif et à pu me livrer le véhicule en une semaine et dans un état irréprochable.",
+      name: "Clement Perret",
       title: "Client satisfait",
     },
     {
-      quote: "Grâce à cette entreprise, j'ai trouvé la voiture d'occasion parfaite à un prix imbattable. Leur équipe est professionnelle, honnête et m'a aidé à chaque étape de l'achat. Une expérience fluide !",
-      name: "Chloé Dubois",
+      quote: "Très contente de l’achat de mon véhicule chez Pierre! En moins d’une semaine, la pépite était entre mes mains :) Pierre, le gérant, est une personne honnête et consciencieuse avec les voitures qu’il vend, et reste très commerçant quand il faut également!",
+      name: "Sara Cardoso",
       title: "Nouvelle propriétaire",
     },
     {
-      quote: "Vendre ma voiture n'a jamais été aussi simple ! L'équipe s'est occupée de tout, de l'évaluation à la paperasse. J'ai obtenu un très bon prix sans effort. Je reviendrai pour ma prochaine vente !",
-      name: "Jean-Pierre Lefevre",
+      quote: "Très bonne experience. Deuxième fois que je passe par Pierre pour la vente et le rachat d'un véhicule. Vente rapide, nouvelle voiture trouvée dans la foulé. Aucune démarche à faire de mon coté, c'est parfait!",
+      name: "Vauquieres Alix",
       title: "Vendeur satisfait",
     },
     {
-      quote: "Ils m'ont proposé une estimation juste et honnête pour ma voiture, avec un processus de vente rapide. J'ai apprécié leur transparence et leur efficacité. Un service de qualité que je recommande.",
-      name: "Sophie Martin",
-      title: "Client heureuse",
+      quote: "Pierre m’a accompagné sur l’achat d’un superbe Q3 Sportback avec LA configuration que je recherchais. Très sympathique, très professionnel, très flexible, il m’a offert un accompagnement vraiment sûr-mesure jusqu’à la réception de ma carte grise définitive ! Je vous le recommande vivement, il sera indispensable pour la recherche de votre prochaine voiture :)",
+      name: "Zakaria Zarid",
+      title: "Client heureux",
     },
     {
-      quote: "Je cherchais une voiture d'occasion fiable, et cette entreprise a surpassé mes attentes. Large choix, bons prix et service client au top. J'ai trouvé exactement ce que je voulais, sans complications.",
-      name: "Maxime Richard",
+      quote: "Très accueillant et professionnel, Pierre sera se montrer à votre écoute et à votre disposition pour l’achat de votre véhicule. Je recommande vivement ses services. Achat d’un BMW X5 M50D dont je suis très satisfait. Bonne route à tous!!!",
+      name: "Benoit Clerc",
       title: "Acheteur satisfait",
     }
   ];
